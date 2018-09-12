485e83ff62e83f9df51a355ade53fd31b4a382e5
Vardhan Bhatt <vardhanvbhatt@gmail.com>
2018-09-12 09:58:49 +0530

    enabling strict mode in notes and todo app

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const noteTitleElement = document.querySelector('#note-title')
 const noteBodyElement = document.querySelector('#note-body')
 const lastUpdatedElement = document.querySelector('#last-edited')
