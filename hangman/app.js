01362e8c1b7dec0e07b6d56514dc763510922790
Vardhan Bhatt <vardhanvbhatt@gmail.com>
2018-09-15 15:59:36 +0530

    Start using http for the data

--- hangman/app.js
+++ hangman/app.js
@@ -13,3 +13,13 @@ window.addEventListener('keypress', (e) => {
     guessWordElement.textContent = game1.puzzle
     remainingGuessElement.textContent = game1.statusMessage
 })
+
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
