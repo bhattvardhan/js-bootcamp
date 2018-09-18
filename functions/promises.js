a25df92bf8780a113065e982e42cca1f5daa01cb
Vardhan Bhatt <vardhanvbhatt@gmail.com>
2018-09-18 14:25:35 +0530

    Promise chaining

--- functions/promises.js
+++ functions/promises.js
@@ -1,28 +1,51 @@
 // Callback
-const getDataCallback = (callback) => {
+const getDataCallback = (number, callback) => {
     setTimeout(() => {
-        callback('This is the callback error', undefined)
+        if (typeof number === 'number') {
+            callback(undefined, number * 2)
+        } else {
+            callback('Number must be provided')
+        }
     }, 2000)
 }
 
-getDataCallback((error, data) => {
+getDataCallback(2, (error, data) => {
     if (error) {
         console.log(error)
-    }else {
-        console.log(data)
+    } else {
+        getDataCallback(data, (error, data) => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log(data)
+            }
+        })
     }
 })
 
 // Promise
-const myPromise = new Promise((resolve, reject) => {
+const getDataPromise = (number) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve('This is the promise data')
-        reject('This is the promise error')
-    }, 2000)
+        typeof number === 'number' ? resolve(number * 2) : reject('Number must be provided')
+    }, 2000);
 })
 
-myPromise.then((data) => {
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}`)
+    }, (error) => {
+        console.log(error)
+    }, (error) => {
+        console.log(error)
+    })
+})
+
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
     console.log(data)
-}, (error) => {
+}).catch((error) => {
     console.log(error)
 })
