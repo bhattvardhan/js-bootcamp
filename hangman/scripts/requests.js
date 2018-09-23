af71a6a8d834d8c2c72a85a3bd204bebd16ea72e
Vardhan Bhatt <vardhanvbhatt@gmail.com>
2018-09-23 23:22:01 +0530

    Making change in the fetch url to make it compatible with http and https

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     if (response.status === 200) {
         const data = await response.json()
         return data.puzzle
@@ -11,7 +11,7 @@ const getPuzzle = async (wordCount) => {
 }
 
 const getCountry = async (countryCode) => {
-    const response = await fetch('http://restcountries.eu/rest/v2/all')
+    const response = await fetch('//restcountries.eu/rest/v2/all')
     if (response.status === 200) {
         const data = await response.json()
         return data.find((country) => country.alpha2Code === countryCode)
@@ -21,7 +21,7 @@ const getCountry = async (countryCode) => {
 }
 
 const getLocation = async () => {
-    const response = await fetch('http://ipinfo.io/json?token=dba6314e827377')
+    const response = await fetch('//ipinfo.io/json?token=dba6314e827377')
     if (response.status === 200) {
         return response.json()
     } else {
