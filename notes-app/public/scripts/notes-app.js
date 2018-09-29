68dc3bb0fb6400c2b7ed220ed310c92b99e1ec10
Vardhan Bhatt <vardhanvbhatt@gmail.com>
2018-09-29 22:59:57 +0530

    Converting notes app to modules

--- notes-app/public/scripts/notes-app.js
+++ notes-app/public/scripts/notes-app.js
@@ -1,41 +1,42 @@
-'use strict'
+"use strict";
 
-let notes = getSavedNotes()
-const timestamp = moment().valueOf()
+let notes = getSavedNotes();
 
 const filters = {
-    searchText: '',
-    sortBy: 'byEdited'
-}
+  searchText: "",
+  sortBy: "byEdited"
+};
 
-renderNotes(notes, filters)
+renderNotes(notes, filters);
 
-document.querySelector('#create-note').addEventListener('click', (e) => {
-    const uniqueID = uuidv4()
-    notes.push({
-        id: uniqueID,
-        title: '',
-        body: '',
-        createdAt: timestamp,
-        updatedAt: timestamp
-    })
-    saveNotes(notes)
-    location.assign(`/edit.html#${uniqueID}`)
-})
+document.querySelector("#create-note").addEventListener("click", e => {
+  const uniqueID = uuidv4();
+  const timestamp = moment().valueOf();
 
-document.querySelector('#search-text').addEventListener('input', (e) => {
-    filters.searchText = e.target.value
-    renderNotes(notes, filters)
-})
+  notes.push({
+    id: uniqueID,
+    title: "",
+    body: "",
+    createdAt: timestamp,
+    updatedAt: timestamp
+  });
+  saveNotes(notes);
+  location.assign(`/edit.html#${uniqueID}`);
+});
 
-document.querySelector('#filter-by').addEventListener('change', (e) => {
-    filters.sortBy = e.target.value
-    renderNotes(notes, filters)
-})
+document.querySelector("#search-text").addEventListener("input", e => {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
 
-window.addEventListener('storage', (e) => {
-    if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        renderNotes(notes, filters)
-    }
-})
+document.querySelector("#filter-by").addEventListener("change", e => {
+  filters.sortBy = e.target.value;
+  renderNotes(notes, filters);
+});
+
+window.addEventListener("storage", e => {
+  if (e.key === "notes") {
+    notes = JSON.parse(e.newValue);
+    renderNotes(notes, filters);
+  }
+});
--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,1 +1,21 @@
-console.log("index.js");
+"use strict";
+
+import { getNotes, createNote, removeNote, updateNote } from "./notes";
+import { getFilters, setFilters } from "./filters";
+// console.log(getNotes());
+
+// createNote();
+
+// removeNote();
+
+// updateNote("17003f00-3ac6-456c-b284-fd6d01bb3d50", {
+//     title: "My note title",
+//     body: "Some body loves you"
+// });
+
+console.log(getFilters());
+setFilters({
+  searchText: "Office",
+  sortBy: "byCreated"
+});
+console.log(getFilters());
